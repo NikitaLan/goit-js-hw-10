af0b5f2db6952f9b187775f1ed3989e1aebd30f2
NikitaLan <114763284+NikitaLan@users.noreply.github.com>
2023-03-26 16:11:40 +0200

    Finish

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const containerGallery = document.querySelector('.gallery');
+
+function createGalleryMarkup(item) {
+  return item
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+      <img loading="lazy" class="gallery__image" src="${preview}" 
+      alt="${description}" width="350"
+      height="240"" style="display: block"/>
+    </a> 
+    `
+    )
+    .join('');
+}
+containerGallery.insertAdjacentHTML(
+  'beforeend',
+  createGalleryMarkup(galleryItems)
+);
+const modalLightbox = new SimpleLightbox('.gallery .gallery__item', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
